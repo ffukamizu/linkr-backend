6c81b7d50a25d3ab97f710c90185c6db0d42bf6e
breno.breves <brenocsb@dcc.ufrj.br>
2023-08-15 20:35:27 -0300

    Repository pattern aplicado e conexão com db validada

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import {db} from "../database/database.connection.js";
+import { validateMailRepo, insertUserRepo } from '../repositories/user.repository.js';
 
 export async function signIn(req,res){
     res.send("Sign-in")
@@ -8,11 +8,11 @@ export async function signIn(req,res){
 export async function signUp(req,res){
     const {name, email, password, photo} = req.body
     try {
-        //const validateMail = await db.query(`SELECT * FROM users WHERE email=$1;`,[email])
-        //if(validateMail.rowCount !== 0) return res.status(409).send("Email já cadastrado")
+        const validateMail = await validateMailRepo(email)
+        if(validateMail.rowCount !== 0) return res.status(409).send("Email já cadastrado")
         const cryptPassword = bcrypt.hashSync(password, 10)
-        //const insertUser = await db.query(`INSERT INTO users (name,email,password,photo) VALUES ($1,$2,$3);`,[name,email,cryptPassword,photo])
-        res.status(201).send("Sign-up")
+        const insertUser = await insertUserRepo(name, email, cryptPassword, photo)
+        res.status(201).send(insertUser)
     } catch (err) {
         res.status(500).send(err.message)
     }
