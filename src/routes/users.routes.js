0dc10437e04c57bf97d16c9b9e246e2693b94a2a
José Matheus <XxFULLDLCxX@outlook.com>
2023-08-21 14:42:51 -0300

    feat: implement a GET user/:id RETURN a username

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { validateSchema } from '../middlewares/validateSchema.middleware.js';
 import { signupSchema } from '../schemas/signup.schema.js';
 import { signinSchema } from '../schemas/signin.schema.js';
-import { signIn, signUp } from '../controllers/users.controllers.js';
+import { getUserById, signIn, signUp } from '../controllers/users.controllers.js';
+import validateAuth from '../middlewares/validateAuth.middlewares.js';
 
 const usersRouter = Router();
 
 usersRouter.post('/signup', validateSchema(signupSchema), signUp);
 usersRouter.post('/signin', validateSchema(signinSchema), signIn);
+usersRouter.get('/user/:id', validateAuth, getUserById)
 
 export default usersRouter;
